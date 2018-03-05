5665481dae64a752b7d2b7dbc5e2d42df1b85ebf
zhoufanz <1026102715@qq.com>
2018-03-05 15:43:56 +0800

    添加切面日志

---
+++ src/main/java/com/example/demo/aspect/HttpAspect.java
@@ -0,0 +1,42 @@
+package com.example.demo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpAspect {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.example.demo.GirlController.*(..))")
+    public void log(){
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        LOGGER.info("method={}",request.getMethod());
+        LOGGER.info("ip={}",request.getRemoteAddr());
+        LOGGER.info("开始");
+
+    }
+
+    @After("log()")
+    public void doAfter() {
+        LOGGER.info("结束");
+    }
+}
